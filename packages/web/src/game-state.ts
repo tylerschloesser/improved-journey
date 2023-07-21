f39ee85d38f2536fab230a4caeed4c84a0fb9618
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-21 12:51:13 -0700

    update dampen logic

--- packages/web/src/game-state.ts
+++ packages/web/src/game-state.ts
@@ -15,6 +15,23 @@ export class Vec2 {
     return new Vec2(this.x + v.x, this.y + v.y)
   }
 
+  mul(s: number): Vec2 {
+    return new Vec2(this.x * s, this.y * s)
+  }
+
+  div(s: number): Vec2 {
+    return new Vec2(this.x / s, this.y / s)
+  }
+
+  len(): number {
+    return Math.sqrt(this.x ** 2 + this.y ** 2)
+  }
+
+  norm(): Vec2 {
+    const l = this.len()
+    return new Vec2(this.x / l, this.y / l)
+  }
+
   toString() {
     return `[${this.x.toFixed(0)},${this.y.toFixed(0)}]`
   }
--- packages/web/src/input.ts
+++ packages/web/src/input.ts
@@ -4,16 +4,7 @@ function toVec2(ev: PointerEvent): Vec2 {
   return new Vec2(ev.clientX, ev.clientY)
 }
 
-function handleMove({
-  prev,
-  next,
-}: {
-  prev: PointerEvent
-  next: PointerEvent
-}) {
-  const d = toVec2(next).sub(toVec2(prev))
-  gameState.position = gameState.position.add(d)
-}
+type PointerId = number
 
 export function initInput({
   canvas,
@@ -22,7 +13,12 @@ export function initInput({
   canvas: HTMLCanvasElement
   signal: AbortSignal
 }) {
-  const pointerCache = new Map<number, PointerEvent>()
+  const pointerCache = new Map<PointerId, PointerEvent>()
+
+  let lastPositionChange: {
+    v: Vec2
+    time: number
+  } | null = null
 
   canvas.addEventListener(
     'pointermove',
@@ -31,8 +27,33 @@ export function initInput({
       pointerCache.set(ev.pointerId, ev)
 
       if (prev && prev.pressure > 0 && ev.pressure > 0) {
-        handleMove({ prev, next: ev })
+        const dp = toVec2(ev).sub(toVec2(prev)).mul(-1)
+        gameState.position = gameState.position.add(dp)
+
+        const dt = ev.timeStamp - prev.timeStamp
+
+        // pointer velocity, in pixels/ms
+        const v = dp.div(dt)
+
+        lastPositionChange = {
+          v,
+          time: ev.timeStamp,
+        }
       }
+    },
+    { signal },
+  )
+
+  canvas.addEventListener(
+    'pointerup',
+    (ev) => {
+      if (lastPositionChange === null) return
+
+      const dt = ev.timeStamp - lastPositionChange.time
+
+      if (dt > 100) return
+
+      console.log('todo dampen')
     },
     { signal },
   )
