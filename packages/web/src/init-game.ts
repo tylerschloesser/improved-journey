d8951a850d547e91e320ea895012ad2e5d3102d5
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-27 12:15:28 -0700

    add missing file

---
+++ packages/web/src/init-game.ts
@@ -0,0 +1,23 @@
+import { Application, Graphics, ICanvas } from 'pixi.js'
+import { combineLatest } from 'rxjs'
+import { cellSize$, miner$, position$ } from './game-state.js'
+
+export function initGame({ app }: { app: Application<ICanvas> }) {
+  const rect = new Graphics()
+  app.stage.addChild(rect)
+
+  rect.beginFill('red')
+  rect.drawRect(0, 0, 100, 100)
+
+  cellSize$.subscribe((cellSize) => {
+    rect.width = rect.height = cellSize
+  })
+
+  combineLatest([miner$, position$, cellSize$]).subscribe(
+    ([miner, position, cellSize]) => {
+      const translate = miner.add(position).mul(cellSize)
+      console.log(translate)
+      rect.position.set(translate.x, translate.y)
+    },
+  )
+}
