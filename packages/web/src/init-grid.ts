ad333aa95bca2bb10af14610cdbfd39b379f0ebc
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-27 10:45:50 -0700

    refactor more

--- packages/web/src/init-grid.ts
+++ packages/web/src/init-grid.ts
@@ -62,40 +62,50 @@ function initChunkGrid({
   const container = new Container()
   app.stage.addChild(container)
 
-  let lines = new Graphics()
-  container.addChild(lines)
+  let lines: Graphics | null = null
 
-  lines.lineStyle(2, 'hsl(0, 0%, 30%)')
   const cellSize = 40
   const chunkSize = 10
 
-  const rows = Math.ceil(rect.height / cellSize / chunkSize) + 1
-  const cols = Math.ceil(rect.width / cellSize / chunkSize) + 1
-
-  for (let x = 0; x < cols; x++) {
-    lines
-      .moveTo(x * cellSize * chunkSize, 0)
-      .lineTo(x * cellSize * chunkSize, cellSize * chunkSize * rows)
-  }
-
-  for (let y = 0; y < rows; y++) {
-    lines
-      .moveTo(0, y * cellSize * chunkSize)
-      .lineTo(cellSize * chunkSize * cols, y * cellSize * chunkSize)
-  }
-
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-      const text = new Text(`[${x},${y}]`, {
-        fontFamily: 'monospace',
-        fill: 'hsl(0, 0%, 30%)',
-      })
-      text.x = x * cellSize * chunkSize
-      text.y = y * cellSize * chunkSize
-
-      container.addChild(text)
+  zip(viewport$, zoom$).subscribe(([viewport, zoom]) => {
+    if (lines) {
+      container.removeChild(lines)
+      lines.destroy()
     }
-  }
+
+    lines = new Graphics()
+    container.addChild(lines)
+
+    lines.lineStyle(2, 'hsl(0, 0%, 30%)')
+
+    const rows = Math.ceil(rect.height / cellSize / chunkSize) + 1
+    const cols = Math.ceil(rect.width / cellSize / chunkSize) + 1
+
+    for (let x = 0; x < cols; x++) {
+      lines
+        .moveTo(x * cellSize * chunkSize, 0)
+        .lineTo(x * cellSize * chunkSize, cellSize * chunkSize * rows)
+    }
+
+    for (let y = 0; y < rows; y++) {
+      lines
+        .moveTo(0, y * cellSize * chunkSize)
+        .lineTo(cellSize * chunkSize * cols, y * cellSize * chunkSize)
+    }
+  })
+
+  // for (let x = 0; x < cols; x++) {
+  //   for (let y = 0; y < rows; y++) {
+  //     const text = new Text(`[${x},${y}]`, {
+  //       fontFamily: 'monospace',
+  //       fill: 'hsl(0, 0%, 30%)',
+  //     })
+  //     text.x = x * cellSize * chunkSize
+  //     text.y = y * cellSize * chunkSize
+
+  //     container.addChild(text)
+  //   }
+  // }
 
   position$.subscribe((position) => {
     container.position.set(
