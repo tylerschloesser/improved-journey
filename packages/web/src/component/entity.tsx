899da201004ad757dd6c6005c4855787ebb33ae4
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-28 19:20:21 -0700

    add container style

--- packages/web/src/component/entity.tsx
+++ packages/web/src/component/entity.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import invariant from 'tiny-invariant'
+import { EntityId } from '../game-state.js'
+
+import styles from './entity.module.scss'
+
+function useEntityId() {
+  const params = useParams<{ id: EntityId }>()
+  invariant(params.id)
+  return params.id
+}
 
 export function Entity() {
-  return <>TODO</>
+  const entityId = useEntityId()
+
+  return <div className={styles.container}>TODO: ${entityId}</div>
 }
