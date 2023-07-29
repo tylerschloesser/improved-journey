a72289deb53d5f95c83116abd79bb1229558f691
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-29 10:15:50 -0700

    set connection entity id

--- packages/web/src/component/connection.tsx
+++ packages/web/src/component/connection.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react'
 
 import styles from './connection.module.scss'
 import { useEntityId } from './use-entity-id.js'
+import { connection$ } from '../game-state.js'
 
 export function Connection() {
   const entityId = useEntityId()
 
   useEffect(() => {
-    console.log(entityId)
+    connection$.next({ entityId })
   }, [])
 
   return <div className={styles.container}>TODO</div>
