0c71f9a8614e61ad2e40f8626e985509f4484d5e
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-29 11:08:56 -0700

    refactor to use container

--- packages/web/src/init-connection.ts
+++ packages/web/src/init-connection.ts
@@ -7,12 +7,13 @@ import {
   worldToScreen$,
 } from './game-state.js'
 import { Vec2 } from './vec2.js'
-import { Graphics } from 'pixi.js'
+import { Container, Graphics } from 'pixi.js'
 import invariant from 'tiny-invariant'
 import { ZIndex } from './z-index.js'
 
 export function initConnection({ app }: InitArgs) {
-  let g: Graphics | null = null
+  let container: Container | null = null
+  // let g: Graphics | null = null
 
   const entity$ = combineLatest([connection$, entities$]).pipe(
     map(([connection, entities]) =>
@@ -35,28 +36,31 @@ export function initConnection({ app }: InitArgs) {
   combineLatest([points$, worldToScreen$, cellSize$]).subscribe(
     ([points, worldToScreen, cellSize]) => {
       if (points === null) {
-        if (g) {
-          app.stage.removeChild(g)
-          g.destroy()
-          g = null
+        if (container) {
+          app.stage.removeChild(container)
+          container.destroy({ children: true })
+          container = null
         }
         return
       }
 
-      if (g === null) {
-        g = new Graphics()
+      if (container === null) {
+        container = new Container()
+        container.zIndex = ZIndex.Connection
+        app.stage.addChild(container)
+
+        const g = new Graphics()
         g.lineStyle(0.1, 'orange')
         g.drawRect(0.05, 0.05, 1, 1)
-        g.zIndex = ZIndex.Connection
-        app.stage.addChild(g)
+        container.addChild(g)
       }
 
       const first = points[0]
       invariant(first)
 
       const { x, y } = worldToScreen(first)
-      g.position.set(x, y)
-      g.width = g.height = cellSize
+      container.position.set(x, y)
+      container.width = container.height = cellSize
     },
   )
 }
