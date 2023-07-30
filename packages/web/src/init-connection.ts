f48f83ccb6f1678952f5ad50a3f5c40a8a435263
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-29 19:14:23 -0700

    refactor connection

--- packages/web/src/init-connection.ts
+++ packages/web/src/init-connection.ts
@@ -23,57 +23,64 @@ export function initConnection({ app }: InitArgs) {
 
   const config$ = entity$.pipe(
     map((entity) => {
-      if (entity === null) {
-        if (container) {
-          app.stage.removeChild(container)
-          container.destroy({ children: true })
-          container = null
-        }
-        return null
+      if (entity === null) return null
+
+      const cells: Vec2[] = []
+      for (let x = 0; x < entity.size.x; x++) {
+        cells.push(new Vec2(1 + x, 0))
+        cells.push(new Vec2(1 + x, entity.size.y + 1))
+      }
+      for (let y = 0; y < entity.size.y; y++) {
+        cells.push(new Vec2(0, 1 + y))
+        cells.push(new Vec2(entity.size.x + 1, 1 + y))
       }
 
-      const config = {
-        position: entity.position.sub(new Vec2(1, 1)),
-        size: entity.size.add(new Vec2(2, 2)),
+      return {
+        cells,
+        bb: {
+          position: entity.position.sub(new Vec2(1, 1)),
+          size: entity.size.add(new Vec2(2, 2)),
+        },
       }
-
-      if (container === null) {
-        container = new Container()
-        container.zIndex = ZIndex.Connection
-        app.stage.addChild(container)
-
-        const points: Vec2[] = []
-        for (let x = 0; x < entity.size.x; x++) {
-          points.push(new Vec2(1 + x, 0))
-          points.push(new Vec2(1 + x, entity.size.y + 1))
-        }
-        for (let y = 0; y < entity.size.y; y++) {
-          points.push(new Vec2(0, 1 + y))
-          points.push(new Vec2(entity.size.x + 1, 1 + y))
-        }
-
-        const g = new Graphics()
-        container.addChild(g)
-
-        const scale = 10
-
-        // TODO fix this is dumb as fuck
-        // force the container to have a size that makes positioning easier
-        // but transparent doesn't work...
-        g.beginFill('hsla(0, 100%, 50%, .0001)')
-        g.drawRect(0, 0, config.size.x * scale, config.size.y * scale)
-
-        const r = 0.25 * scale
-        g.beginFill('hsl(0, 0%, 50%)')
-
-        for (const { x, y } of points.slice(0, 8)) {
-          g.drawCircle((x + 0.5) * scale, (y + 0.5) * scale, r)
-        }
-      }
-
-      return config
     }),
   )
+
+  config$.subscribe((config) => {
+    if (config === null) {
+      if (container) {
+        app.stage.removeChild(container)
+        container.destroy({ children: true })
+        container = null
+      }
+      return
+    }
+
+    const { cells, bb } = config
+
+    if (container === null) {
+      container = new Container()
+      container.zIndex = ZIndex.Connection
+      app.stage.addChild(container)
+
+      const g = new Graphics()
+      container.addChild(g)
+
+      const scale = 10
+
+      // TODO fix this is dumb as fuck
+      // force the container to have a size that makes positioning easier
+      // but transparent doesn't work...
+      g.beginFill('hsla(0, 100%, 50%, .0001)')
+      g.drawRect(0, 0, bb.size.x * scale, bb.size.y * scale)
+
+      const r = 0.25 * scale
+      g.beginFill('hsl(0, 0%, 50%)')
+
+      for (const { x, y } of cells.slice(0, 8)) {
+        g.drawCircle((x + 0.5) * scale, (y + 0.5) * scale, r)
+      }
+    }
+  })
 
   combineLatest([config$, worldToScreen$, cellSize$]).subscribe(
     ([config, worldToScreen, cellSize]) => {
@@ -81,10 +88,10 @@ export function initConnection({ app }: InitArgs) {
         return
       }
 
-      const { x, y } = worldToScreen(config.position)
+      const { x, y } = worldToScreen(config.bb.position)
       container.position.set(x, y)
-      container.width = config.size.x * cellSize
-      container.height = config.size.y * cellSize
+      container.width = config.bb.size.x * cellSize
+      container.height = config.bb.size.y * cellSize
     },
   )
 }
