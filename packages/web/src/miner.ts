de5c04ae3af268bc6891933036fa9f2ff388bb50
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-31 10:47:27 -0700

    refactor build nodes

--- packages/web/src/miner.ts
+++ packages/web/src/miner.ts
@@ -1,6 +1,34 @@
 import { Entity, EntityNode } from './game-state.js'
+import { Vec2 } from './vec2.js'
 
 export function newMiner(args: Omit<Entity, 'nodes'>): Entity {
+  return { ...args, nodes: buildNodes(args) }
+}
+
+function buildNodes({ size }: { size: Vec2 }): EntityNode[] {
   const nodes: EntityNode[] = []
-  return { ...args, nodes }
+
+  for (let x = 0; x < size.x; x++) {
+    nodes.push(
+      {
+        position: new Vec2(x, -1),
+      },
+      {
+        position: new Vec2(x, size.y),
+      },
+    )
+  }
+
+  for (let y = 0; y < size.y; y++) {
+    nodes.push(
+      {
+        position: new Vec2(-1, y),
+      },
+      {
+        position: new Vec2(size.x, y),
+      },
+    )
+  }
+
+  return nodes
 }
