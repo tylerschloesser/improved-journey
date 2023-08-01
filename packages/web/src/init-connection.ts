131497b9ea010cbaf8823de7c1fbd1121225c6d4
Tyler Schloesser <tylerschloesser@gmail.com>
2023-07-31 21:32:43 -0700

    cleanup

--- packages/web/src/init-connection.ts
+++ packages/web/src/init-connection.ts
@@ -1,14 +1,6 @@
 import { isEqual } from 'lodash-es'
 import { Container, Graphics } from 'pixi.js'
-import {
-  BehaviorSubject,
-  combineLatest,
-  distinctUntilChanged,
-  map,
-  ReplaySubject,
-  Subject,
-  tap,
-} from 'rxjs'
+import { BehaviorSubject, combineLatest, distinctUntilChanged, map } from 'rxjs'
 import invariant from 'tiny-invariant'
 import {
   cellSize$,
@@ -16,12 +8,12 @@ import {
   entities$,
   Entity,
   EntityNode,
+  PIXI,
   position$,
   worldToScreen$,
 } from './game-state.js'
 import { InitArgs } from './init-args.js'
 import { Vec2 } from './vec2.js'
-import { ZIndex } from './z-index.js'
 
 const CONNECTION_POINT_RADIUS = 0.166
 const SCALE = 10
@@ -29,15 +21,10 @@ const SCALE = 10
 interface State {
   root: Container
   container: Container
-  belt: Graphics
   g: {
     points: Graphics
     selected: Graphics
   }
-}
-
-interface BeltState {
-  g: Graphics
 }
 
 interface Selected {
@@ -61,20 +48,14 @@ export function initConnection({ app }: InitArgs) {
       const state = state$.value
 
       if (state !== null) {
-        app.stage.removeChild(state.container)
+        PIXI.container.world.removeChild(state.container)
         state.container.destroy({ children: true })
-
-        app.stage.removeChild(state.belt)
-        state.belt.destroy()
       }
 
       if (entity === null) {
         state$.next(null)
         return
       }
-
-      const belt = new Graphics()
-      app.stage.addChild(belt)
 
       const container = new Container()
       app.stage.addChild(container)
@@ -117,7 +98,7 @@ export function initConnection({ app }: InitArgs) {
         g.selected.drawCircle(0.5 * SCALE, 0.5 * SCALE, r)
       }
 
-      state$.next({ root: app.stage, container, g, belt })
+      state$.next({ root: app.stage, container, g })
     })
 
   const selected$ = combineLatest([entity$, position$]).pipe(
@@ -143,77 +124,12 @@ export function initConnection({ app }: InitArgs) {
     distinctUntilChanged<Selected | null>(isEqual),
   )
 
-  const beltGraphics$ = new BehaviorSubject<Graphics | null>(null)
-
-  const belt$ = combineLatest([
-    state$,
-    selected$,
-    position$.pipe(
-      map((position) => position.floor()),
-      distinctUntilChanged<Vec2>(isEqual),
-    ),
-  ]).pipe(
-    map(([state, selected, position]) => {
-      if (!state || !selected) return null
-
-      const dp = position.sub(
-        selected.node.position.add(selected.entity.position),
-      )
-
-      for (let x = 1; x < Math.abs(dp.x); x++) {}
-
-      return [position]
-    }),
-    tap((belt) => {
-      let g = beltGraphics$.value
-      if (g) {
-        app.stage.removeChild(g)
-        g.destroy()
-      }
-      if (!belt) {
-        beltGraphics$.next(null)
-        return
-      }
-
-      g = new Graphics()
-      g.zIndex = ZIndex.Belt
-
-      g.beginFill('pink')
-      for (const cell of belt) {
-        g.drawRect(cell.x, cell.y, 1, 1)
-      }
-
-      beltGraphics$.next(g)
-    }),
-  )
-
-  combineLatest([belt$, beltGraphics$, worldToScreen$, cellSize$]).subscribe(
-    ([belt, beltGraphics, worldToScreen, cellSize]) => {
-      if (!beltGraphics) {
-        return
-      }
-
-      const { x, y } = worldToScreen()
-      beltGraphics.position.set(x, y)
-      beltGraphics.width = cellSize
-      beltGraphics.height = cellSize
-    },
-  )
-
   combineLatest([state$, selected$]).subscribe(([state, selected]) => {
     if (!state || !selected) return
 
     const { x, y } = selected.node.position.mul(SCALE)
     state.g.selected.position.set(x, y)
   })
-
-  combineLatest([state$, worldToScreen$, cellSize$]).subscribe(
-    ([state, worldToScreen, cellSize]) => {
-      if (state === null) return
-      const { x, y } = worldToScreen()
-      state.belt.position.set(x, y)
-    },
-  )
 
   combineLatest([entity$, state$, worldToScreen$, cellSize$]).subscribe(
     ([entity, state, worldToScreen, cellSize]) => {
