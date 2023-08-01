b55556dec4c7c2b01831e2ee0d419b2b4f9836b2
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-01 14:58:41 -0700

    build belts

--- packages/web/src/component/connection.tsx
+++ packages/web/src/component/connection.tsx
@@ -2,8 +2,14 @@ import React, { useEffect } from 'react'
 
 import styles from './connection.module.scss'
 import { useEntityId } from './use-entity-id.js'
-import { buildConnection$, connection$ } from '../game-state.js'
+import {
+  buildConnection$,
+  connection$,
+  entities$,
+  nextEntityId$,
+} from '../game-state.js'
 import { bind } from '@react-rxjs/core'
+import invariant from 'tiny-invariant'
 
 const [useBuildConnection] = bind(buildConnection$)
 
@@ -27,7 +33,14 @@ export function Connection() {
         disabled={!valid}
         onPointerUp={() => {
           if (!valid) return
-          console.log('todo build')
+          invariant(build)
+
+          const entities = { ...entities$.value }
+          for (const cell of build.cells) {
+            entities[cell.entity.id] = cell.entity
+          }
+          entities$.next(entities)
+          nextEntityId$.next(build.nextEntityId)
         }}
       >
         Build
