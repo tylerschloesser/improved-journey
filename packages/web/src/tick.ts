ae732c8360c03493c7912a7536883c185297a0d1
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-02 16:01:06 -0700

    compute basic satisifaction

--- packages/web/src/tick.ts
+++ packages/web/src/tick.ts
@@ -1,10 +1,26 @@
 import { cloneDeep } from 'lodash-es'
 import { world$ } from './game-state.js'
+import { EntityType } from './types.js'
 
 export function tickWorld() {
   const world = cloneDeep(world$.value)
 
-  world.tick += 1
+  let consumption = 0
+  let production = 0
 
+  for (const entity of Object.values(world.entities)) {
+    switch (entity.type) {
+      case EntityType.Miner:
+        consumption += 1
+        break
+      case EntityType.Generator:
+        production += 2
+        break
+    }
+  }
+
+  const satisfaction = consumption === 0 ? null : production / consumption
+
+  world.tick += 1
   world$.next(world)
 }
