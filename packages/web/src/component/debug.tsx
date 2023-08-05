cf3820bedab92ef4d2159c49887512114ddfaeb6
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-04 17:49:10 -0700

    debug styles

--- packages/web/src/component/debug.tsx
+++ packages/web/src/component/debug.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { BackButton } from './back-button.js'
+
+import styles from './debug.module.scss'
 
 export function Debug() {
-  return <>todo</>
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}></div>
+      <div className={styles.controls}>
+        <BackButton className={styles.button} />
+      </div>
+    </div>
+  )
 }
