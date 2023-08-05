badd44052df9f902aad975a86dde86afea0967c0
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-04 22:21:29 -0700

    call worker

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -6,6 +6,8 @@ import invariant from 'tiny-invariant'
 import './index.scss'
 import { router } from './router.js'
 
+const worker = new Worker(new URL('./worker/index.js', import.meta.url))
+
 const container = document.getElementById('root')
 invariant(container)
 const root = createRoot(container)
