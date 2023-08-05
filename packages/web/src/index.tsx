e3364a089a8add3e209af241026170550876c5d5
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-04 22:19:20 -0700

    refactor router

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,57 +1,14 @@
+import { Subscribe } from '@react-rxjs/core'
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import invariant from 'tiny-invariant'
-import { World } from './component/world.js'
 import './index.scss'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { WorldControls } from './component/world-controls.js'
-import { Entity } from './component/entity.js'
-import { Subscribe } from '@react-rxjs/core'
-import { Connection } from './component/connection.js'
-import { BuildEntity } from './component/build-entity.js'
-import { Build } from './component/build.js'
-import { Debug } from './component/debug.js'
+import { router } from './router.js'
 
 const container = document.getElementById('root')
 invariant(container)
 const root = createRoot(container)
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>TODO add root/home</div>,
-  },
-  {
-    path: '/world/:id',
-    element: <World />,
-    children: [
-      {
-        path: '',
-        element: <WorldControls />,
-      },
-      {
-        path: 'debug',
-        element: <Debug />,
-      },
-      {
-        path: 'build',
-        element: <Build />,
-      },
-      {
-        path: 'build/:type',
-        element: <BuildEntity />,
-      },
-      {
-        path: 'entity/:id',
-        element: <Entity />,
-      },
-      {
-        path: 'entity/:id/connection',
-        element: <Connection />,
-      },
-    ],
-  },
-])
 
 root.render(
   <StrictMode>
---
+++ packages/web/src/router.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import { BuildEntity } from './component/build-entity.js'
+import { Build } from './component/build.js'
+import { Connection } from './component/connection.js'
+import { Debug } from './component/debug.js'
+import { Entity } from './component/entity.js'
+import { WorldControls } from './component/world-controls.js'
+import { World } from './component/world.js'
+import './index.scss'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>TODO add root/home</div>,
+  },
+  {
+    path: '/world/:id',
+    element: <World />,
+    children: [
+      {
+        path: '',
+        element: <WorldControls />,
+      },
+      {
+        path: 'debug',
+        element: <Debug />,
+      },
+      {
+        path: 'build',
+        element: <Build />,
+      },
+      {
+        path: 'build/:type',
+        element: <BuildEntity />,
+      },
+      {
+        path: 'entity/:id',
+        element: <Entity />,
+      },
+      {
+        path: 'entity/:id/connection',
+        element: <Connection />,
+      },
+    ],
+  },
+])
