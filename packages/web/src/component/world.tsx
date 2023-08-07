cd8e5b380890c585f7a46e9e525b64826108d598
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-06 17:44:07 -0700

    refactor pixi world

--- packages/web/src/component/world.tsx
+++ packages/web/src/component/world.tsx
@@ -1,23 +1,12 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './world.module.scss'
 
-import { Container, Stage } from '@pixi/react'
-import { bind, Subscribe } from '@react-rxjs/core'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { TICK_RATE } from '../const.js'
-import {
-  cellSize$,
-  navigate$,
-  position$,
-  viewport$,
-  world$,
-} from '../game-state.js'
+import { navigate$, viewport$, world$ } from '../game-state.js'
 import { init } from '../init.js'
+import { World as PixiWorld } from '../pixi/world.js'
 import { Vec2 } from '../vec2.js'
-import { Grid } from './grid.js'
-import { Entities } from './entities.js'
-import { Build } from '../pixi/build.js'
-import { Connection } from '../pixi/connection.js'
 import { worker } from '../worker.js'
 
 function useResizeObserver(canvas: HTMLCanvasElement | null) {
@@ -88,33 +77,6 @@ function useTickWorld() {
   }, [])
 }
 
-const [usePosition] = bind(position$)
-const [useCellSize] = bind(cellSize$)
-const [useViewport] = bind(viewport$)
-
-function useTransform() {
-  const position = usePosition()
-  const cellSize = useCellSize()
-  const viewport = useViewport()
-  return {
-    translate: position.mul(cellSize * -1).add(viewport.div(2)),
-    scale: cellSize,
-  }
-}
-
-function Transform({ children }: React.PropsWithChildren) {
-  const transform = useTransform()
-  return (
-    <Container
-      x={transform.translate.x}
-      y={transform.translate.y}
-      scale={transform.scale}
-    >
-      {children}
-    </Container>
-  )
-}
-
 export function World() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
   useResizeObserver(canvas)
@@ -124,34 +86,7 @@ export function World() {
 
   return (
     <div className={styles.container}>
-      {canvas && (
-        <Stage
-          options={{
-            resizeTo: window,
-            view: canvas,
-          }}
-          onMount={(app) => {
-            // hack because @pixi/react doesn't
-            // respect my initial size...
-            window.setTimeout(() => {
-              app.resize?.()
-            }, 0)
-          }}
-        >
-          {/* TODO I need this subscribe here even though I already
-        have on in index. Not sure why.. something to do with @pixi/react */}
-          <Subscribe>
-            <Suspense>
-              <Grid />
-              <Transform>
-                <Entities />
-                <Build />
-                <Connection />
-              </Transform>
-            </Suspense>
-          </Subscribe>
-        </Stage>
-      )}
+      {canvas && <PixiWorld canvas={canvas} />}
       <canvas className={styles.canvas} ref={setCanvas}></canvas>
       <div className={styles.outlet}>
         <Outlet />
---
+++ packages/web/src/pixi/world.tsx
@@ -0,0 +1,71 @@
+import React, { Suspense } from 'react'
+
+import { Container, Stage } from '@pixi/react'
+import { bind, Subscribe } from '@react-rxjs/core'
+import { Entities } from '../component/entities.js'
+import { Grid } from '../component/grid.js'
+import { cellSize$, position$, viewport$ } from '../game-state.js'
+import { Build } from './build.js'
+import { Connection } from './connection.js'
+
+const [usePosition] = bind(position$)
+const [useCellSize] = bind(cellSize$)
+const [useViewport] = bind(viewport$)
+
+function useTransform() {
+  const position = usePosition()
+  const cellSize = useCellSize()
+  const viewport = useViewport()
+  return {
+    translate: position.mul(cellSize * -1).add(viewport.div(2)),
+    scale: cellSize,
+  }
+}
+
+function Transform({ children }: React.PropsWithChildren) {
+  const transform = useTransform()
+  return (
+    <Container
+      x={transform.translate.x}
+      y={transform.translate.y}
+      scale={transform.scale}
+    >
+      {children}
+    </Container>
+  )
+}
+
+export type WorldProps = React.PropsWithoutRef<{
+  canvas: HTMLCanvasElement
+}>
+
+export function World({ canvas }: WorldProps) {
+  return (
+    <Stage
+      options={{
+        resizeTo: window,
+        view: canvas,
+      }}
+      onMount={(app) => {
+        // hack because @pixi/react doesn't
+        // respect my initial size...
+        window.setTimeout(() => {
+          app.resize?.()
+        }, 0)
+      }}
+    >
+      {/* TODO I need this subscribe here even though I already
+        have on in index. Not sure why.. something to do with @pixi/react */}
+      <Subscribe>
+        <Suspense>
+          <Grid />
+          <Transform>
+            <Entities />
+            <Build />
+            <Connection />
+          </Transform>
+        </Suspense>
+      </Subscribe>
+    </Stage>
+  )
+}
