9384d8e11d82749b9ca777c4e1b076caf37db8f3
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-06 17:49:29 -0700

    refactor pixi entities

--- packages/web/src/pixi/entities.tsx
+++ packages/web/src/pixi/entities.tsx
@@ -3,28 +3,32 @@ import { bind } from '@react-rxjs/core'
 import * as PIXI from 'pixi.js'
 import React from 'react'
 import { useCallback } from 'react'
+import { Entity } from '../entity-types.js'
 import { entities$ } from '../game-state.js'
 
 const [useEntities] = bind(entities$)
 
-export function Entities() {
-  const entities = useEntities()
+function Entity({ entity }: { entity: Entity }) {
   const draw = useCallback(
     (g: PIXI.Graphics) => {
       g.clear()
 
-      for (const entity of Object.values(entities)) {
-        g.beginFill(entity.color)
-        g.drawRect(
-          entity.position.x,
-          entity.position.y,
-          entity.size.x,
-          entity.size.y,
-        )
-      }
+      g.beginFill(entity.color)
+      g.drawRect(
+        entity.position.x,
+        entity.position.y,
+        entity.size.x,
+        entity.size.y,
+      )
     },
-    [entities],
+    [entity],
   )
-
   return <Graphics draw={draw} />
+}
+
+export function Entities() {
+  const entities = useEntities()
+  return Object.values(entities).map((entity) => (
+    <Entity key={entity.id} entity={entity} />
+  ))
 }
