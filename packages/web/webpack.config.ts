259862d2c4fd091689e2b1c3807f39f99a9ac0c4
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-12 14:46:20 -0700

    update build

--- packages/web/webpack.config.ts
+++ packages/web/webpack.config.ts
@@ -10,71 +10,79 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const prod = process.env.PROD || !process.env.WEBPACK_SERVE
 
-const config: Configuration = {
-  stats: 'minimal',
-  mode: prod ? 'production' : 'development',
-  entry: './src/index.tsx',
-  devtool: prod ? 'source-map' : 'eval-cheap-module-source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].chunk.js',
-    clean: true,
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                auto: true,
-                localIdentName: '[local]--[hash:base64:5]',
-              },
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [['postcss-preset-env']],
-              },
-            },
-          },
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    extensionAlias: {
-      '.js': ['.ts', '.tsx', '.js'],
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: prod ? 'index.[contenthash].html' : 'index.html',
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    hot: false,
-    watchFiles: ['./src/index.html'],
-    historyApiFallback: {
-      disableDotRule: true,
-    },
-    allowedHosts: ['.amazonaws.com'],
-  },
+interface WebpackEnv {
+  target?: 'github-pages'
 }
 
-export default config
+export default (env: WebpackEnv) => {
+  const config: Configuration = {
+    stats: 'minimal',
+    mode: prod ? 'production' : 'development',
+    entry: './src/index.tsx',
+    devtool: prod ? 'source-map' : 'eval-cheap-module-source-map',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].chunk.js',
+      clean: true,
+      publicPath: env.target === 'github-pages' ? 'improved-journey' : '',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  auto: true,
+                  localIdentName: '[local]--[hash:base64:5]',
+                },
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [['postcss-preset-env']],
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      extensionAlias: {
+        '.js': ['.ts', '.tsx', '.js'],
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename:
+          prod && env.target !== 'github-pages'
+            ? 'index.[contenthash].html'
+            : 'index.html',
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      hot: false,
+      watchFiles: ['./src/index.html'],
+      historyApiFallback: {
+        disableDotRule: true,
+      },
+      allowedHosts: ['.amazonaws.com'],
+    },
+  }
+  return config
+}
