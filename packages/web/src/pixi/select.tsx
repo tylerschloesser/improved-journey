f13d52d7936f3f2f49bdc27ae6860c0ddf80ffd7
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-12 12:03:49 -0700

    add area

--- packages/web/src/pixi/select.tsx
+++ packages/web/src/pixi/select.tsx
@@ -2,6 +2,7 @@ import { Graphics } from '@pixi/react'
 import { bind } from '@react-rxjs/core'
 import { map } from 'rxjs'
 import { position$, select$ } from '../game-state.js'
+import { Vec2 } from '../vec2.js'
 import { useDraw } from './use-draw.js'
 
 const [useSelect] = bind(select$)
@@ -17,22 +18,35 @@ export function Select() {
 
       if (select === null) return
 
-      const lineWidth = 0.1
+      let area: { start: Vec2; end: Vec2 } | null = null
 
-      {
+      if (select.start) {
+        area = {
+          start: new Vec2(
+            Math.min(position.x, select.start.x),
+            Math.min(position.y, select.start.y),
+          ),
+          end: new Vec2(
+            Math.max(position.x, select.start.x),
+            Math.max(position.y, select.start.y),
+          ),
+        }
+      }
+
+      const lineWidth = 0.1
+      g.lineStyle(lineWidth, 'white')
+
+      if (area) {
+        const size = area.end.sub(area.start)
         const { x, y } = position
-        g.lineStyle(lineWidth, 'white')
         g.drawRect(
           x + lineWidth / 2,
           y + lineWidth / 2,
-          1 - lineWidth,
-          1 - lineWidth,
+          size.x - lineWidth,
+          size.y - lineWidth,
         )
-      }
-
-      if (select.start) {
-        const { x, y } = select.start
-        g.lineStyle(lineWidth, 'white')
+      } else {
+        const { x, y } = position
         g.drawRect(
           x + lineWidth / 2,
           y + lineWidth / 2,
