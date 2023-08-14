3600cfd74a90dbbcf0275ec1cafcf2ffbfd5cedc
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-14 13:16:00 -0700

    refactor tick belt

--- packages/web/src/worker/tick.ts
+++ packages/web/src/worker/tick.ts
@@ -60,6 +60,68 @@ const tickMiner: TickEntityFn<MinerEntity> = ({
   }
 }
 
+export const tickBelt: TickEntityFn<BeltEntity> = ({
+  entity,
+  world,
+  satisfaction,
+  moved,
+}) => {
+  const remove = new Set<(typeof entity.items)[0]>()
+  for (const item of entity.items) {
+    // check if the item was just moved onto this belt
+    if (moved.has(item)) continue
+
+    item.progress += BELT_SPEED.perTick() * satisfaction
+    if (item.progress >= 1) {
+      let next: Entity | null = null
+      if (entity.connections.output.size > 0) {
+        invariant(entity.connections.output.size === 1)
+        next = world.entities[Array.from(entity.connections.output)[0]]
+        invariant(next)
+      }
+
+      if (next === null) {
+        item.progress = 1
+      } else {
+        remove.add(item)
+        switch (next.type) {
+          case EntityType.Belt: {
+            moved.add(item)
+            next.items.push(item)
+            item.progress -= 1
+            break
+          }
+          case EntityType.Generator: {
+            let fuel = next.fuel
+            if (!fuel) {
+              fuel = next.fuel = { type: item.type, count: 0 }
+            }
+            invariant(fuel.type === item.type)
+            fuel.count += 1
+            break
+          }
+          case EntityType.Smelter: {
+            let input = next.input
+            if (!input) {
+              input = next.input = { type: item.type, count: 0 }
+            }
+            invariant(input.type === item.type)
+            input.count += 1
+            break
+          }
+          case EntityType.Storage: {
+            next.items[item.type] = (next.items[item.type] ?? 0) + 1
+            break
+          }
+        }
+      }
+    }
+  }
+  if (remove.size) {
+    entity.items = entity.items.filter((item) => !remove.has(item))
+  }
+}
+
 export function tickWorld(world: World): TickStats {
   let { satisfaction } = tickEnergy(world)
 
@@ -80,60 +142,7 @@ export function tickWorld(world: World): TickStats {
         break
       }
       case EntityType.Belt: {
-        const remove = new Set<(typeof entity.items)[0]>()
-        for (const item of entity.items) {
-          // check if the item was just moved onto this belt
-          if (moved.has(item)) continue
-
-          item.progress += BELT_SPEED.perTick() * satisfaction
-          if (item.progress >= 1) {
-            let next: Entity | null = null
-            if (entity.connections.output.size > 0) {
-              invariant(entity.connections.output.size === 1)
-              next = world.entities[Array.from(entity.connections.output)[0]]
-              invariant(next)
-            }
-
-            if (next === null) {
-              item.progress = 1
-            } else {
-              remove.add(item)
-              switch (next.type) {
-                case EntityType.Belt: {
-                  moved.add(item)
-                  next.items.push(item)
-                  item.progress -= 1
-                  break
-                }
-                case EntityType.Generator: {
-                  let fuel = next.fuel
-                  if (!fuel) {
-                    fuel = next.fuel = { type: item.type, count: 0 }
-                  }
-                  invariant(fuel.type === item.type)
-                  fuel.count += 1
-                  break
-                }
-                case EntityType.Smelter: {
-                  let input = next.input
-                  if (!input) {
-                    input = next.input = { type: item.type, count: 0 }
-                  }
-                  invariant(input.type === item.type)
-                  input.count += 1
-                  break
-                }
-                case EntityType.Storage: {
-                  next.items[item.type] = (next.items[item.type] ?? 0) + 1
-                  break
-                }
-              }
-            }
-          }
-        }
-        if (remove.size) {
-          entity.items = entity.items.filter((item) => !remove.has(item))
-        }
+        tickBelt({ entity, world, satisfaction, moved })
         break
       }
       case EntityType.Smelter: {
