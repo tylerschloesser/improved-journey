b9df01ada309709cb1760af3c465e52eb4f2b6c0
Tyler Schloesser <tylerschloesser@gmail.com>
2023-08-14 18:28:13 -0700

    update lab rendering

--- packages/web/src/pixi/lab-entity.tsx
+++ packages/web/src/pixi/lab-entity.tsx
@@ -1,5 +1,9 @@
-import { Container, Graphics } from '@pixi/react'
+import { Container, Graphics, Text } from '@pixi/react'
+import * as PIXI from 'pixi.js'
+import { useMemo } from 'react'
 import { LabEntity } from '../entity-types.js'
+import { Vec2 } from '../vec2.js'
+import { drawItem } from './draw-item.js'
 import { EntityProps } from './entity-props.js'
 import { useDraw } from './use-draw.js'
 import { ZIndex } from './z-index.js'
@@ -17,9 +21,36 @@ export function LabEntity({ entity, config }: EntityProps<LabEntity>) {
     [entity],
   )
 
+  const drawTarget = useDraw(
+    (g) => {
+      g.clear()
+      if (entity.target === null) return
+      drawItem({
+        itemType: entity.target,
+        g,
+        position: new Vec2(entity.position).add(new Vec2(2.5, 0.5)),
+      })
+    },
+    [entity],
+  )
+
+  let progress = null
+  if (entity.progress !== null) {
+    progress = Math.trunc(entity.progress * 100)
+  }
+
+  const textStyle = useMemo(
+    () => new PIXI.TextStyle({ fill: 'black', align: 'center', fontSize: 40 }),
+    [],
+  )
+
   return (
     <Container zIndex={ZIndex.entity}>
-      <Graphics draw={drawBackground} zIndex={ZIndex.entity} />
+      <Graphics draw={drawBackground} />
+      <Container x={x} y={y} width={width} height={height} scale={0.01}>
+        {progress !== null && <Text text={`${progress}%`} style={textStyle} />}
+      </Container>
+      <Graphics draw={drawTarget} />
     </Container>
   )
 }
